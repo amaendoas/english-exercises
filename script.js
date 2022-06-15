8c7e4775f8d4ccb2054a25dfee6e4a13dd9c385c
Amanda Guerra <guerraamanda1@hotmail.com>
2022-06-15 09:57:48 -0300

    Carrossel dos Exercícios

---
+++ script.js
@@ -0,0 +1,10 @@
+/* swiper */
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 2,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  mousewheel: true,
+  keyboard: true,
+});
